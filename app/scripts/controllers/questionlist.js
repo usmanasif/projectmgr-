936edd67fa10ea0bc8dbe39ce6ec16ad5f573789
Im Genius <im.geniusman@gmail.com>
2014-07-01 23:54:58 -0400

    projectmgr initial commit

--- app/scripts/controllers/questionlist.js
+++ app/scripts/controllers/questionlist.js
@@ -58,6 +58,8 @@ angular.module('projectmgrApp')
         SpinningWheel.destroy();
       });
 
+      var newUrl;
+
       $scope.selectAnswer = function(status){
         var answersToUpdate = _.find(answers, function (answ){
           return answ.question_id ==questionId;
@@ -70,8 +72,10 @@ angular.module('projectmgrApp')
           status = 3;
         answersToUpdate.status = status;
         $scope.isQuestionSelected = false;
+        //answersToUpdate.url = "http://www.johnsoncareers.com/favicon.png";  
         console.log(answersToUpdate);
         //{data:{authenticity_token:"f443c742584f5b591d349b9e78f62c77", status: "1"}
+
         Api.patch(settings.url + 'projects/' + $scope.project.id + '/answers/' + answersToUpdate.id,
           {status: answersToUpdate.status })
         .then(function (data){
@@ -81,8 +85,16 @@ angular.module('projectmgrApp')
             console.dir(data);
           } 
         });
+        Api.patch(settings.url + 'projects/' + $scope.project.id + '/answers/' + answersToUpdate.id + '/update_answer', {answer:{notes: $scope.textModel, file: newUrl }})
+        .then(function (data){
+          if (data.error){
+            console.dir(error);
+          } else {
+            console.dir(data);
+          } 
+        });
+        
       };
-
       $scope.changeResolve = function (){
         if ($scope.resolvedStatus == 'Resolved'){
           $scope.resolvedStatus = 'Unresolved';
@@ -104,10 +116,11 @@ angular.module('projectmgrApp')
       $scope.takePic = function (){
         try
         {
+          // uploadPhoto("iVBORw0KGgoAAAANSUhEUgAAACUAAAAhCAYAAABeD2IVAAAFN0lEQVR4Xt2YaUwUZxjHFSxUCm3SStJIEARW7oBNQKhfyCKFVKUlJQEVaKMNbUqxCSDwgVIi8cIq1sYqsQlWqBy1Sq1cC3Is9yK4Qt3lkcNKI7dWLKd8oP/3LZOgzKzbsm3avskvy8zCzm+eeY53WTE/P/+v4/8hNTc3p5Pe3r4V7e03OFjmYC2wAWtw7u+RwnoBbACOYB14BZgvnH9pQcLFyMhIHhYWFnn06OcpWVlZh5OSkvdCTGZwKazVO3fuCkcUstTqm2dLS8sOHj+e+TFWVHR0dERqamr0sWPHP7t06fL5hoZG1fXrbdTael1Aa21tHQQxY0NL2VRWXst4/HiOxJienuECKlUrp6VFJcCP5XJ5KLsxQ0vZIwInpaRmZx9zqebmliUwsYiIiPcgZWFoKRkicJoJSHHrlkZSKiFhXyzLQYNHqrGx6UtdUvfuDUhKZWRkpLBCWLYU1nNgHdgUHPxWaHd3T74uqZmZWUKCi0plZ587aWJiIl9oEUZ/SQrLzsvLa3tZWfmh+/cfKEUk9I6WkOx1dfWNJ058kWZhYbFFKmo6hRDujyYmJttZAv9JeG41NTUv5gm5a9eqFN7em94WE5MSMraysgqEkFpaQDf4W2praxeEqLZWyaL0hFxVVXWVmZkZi9hKfaRWh4eHh4kI8A9Hl6b6+np6+HBcp9jw8AidPfs1RUZGka+vL6HHLYlafHwCr0h9pFaamprKkUcNLHGfAkINFBISQra2tuTn50d79rxPycnJqK6jdOjQYVwonoKDg8ne3p6cnJwoLi6eampquQyqV4Afp6R8Gg8pS0mphbVqgXX796d/ODU1rWEiYqjVai4SHr6DfHx8yM3NjTw8PCggIIAweujChTwaG7uPRzlBQ0PDNDg49DRdgYGB70DqebBqiRRPJGNjuaen57bFnDmTFf/o0W832AgxNJ2dP+W7urpuc3R05EDhRbFIud++3V3wRySYgOGZnJxC1IZIq+1CTrVwVCoVXbx48RwEnMWkXt648bXtCHkzHhstF0SYP76BgUHCPos6OjpRcVUazE8CQm6xfFNJRkrYCaSlpX2Aktayu5Kip6eX/P39WfXQqVNfUVHRD+zDeRFcufIjlZcr0MGzKTPzBJI5hQ1jQiOmI0eOtLLfWQxm4ifsus+qPhl6SCbESApEgSe1g4MD2dnZ0fr163k12tjYcAoLv4OENzvP35fJZPxVoajQsH4lkJ+fnwMhF31agqm5ufmWvr47peziUhw4cJALOTs7C/A2EBQUxPtYXl4eubi4sHNcKDY2lp9XKpUcPMpWJPh2tmPVd8y8il4TipxQj48/IjHQx1hj5BESorV582YqLi4RIoGfiyGzlxITk9B4mUwdfwU4lxgHIVt9x4wMDXC3paXlG2gJ+x48+JV0UVJSShiyVFBQyPsRtstUXV3DQZ4JEvwYacHJzf2WPTZXvQYylgMSNwFR0N6583MlNv87MBKycUwSLBbkxxqNllWZAGSqOcKxQqEQeWzSHX1NVNS7u3C3naOjYwRY1+3IyclNQ34pR0ZGSR9wM1RRUSkJKrMNebaVtQB9pDxv3uwoWjwSIMjBqmbH+oKtCbu4GFzs/Pmcb1jVAWkpZh0TE7O7v/8Xunu3f7nwPdPVq8Vi8ELA1zNKT09PY/1Jl5QMv3wanZc1xmXT1UW8iaKpisHfw/fDJki9rkvKG1+PqthMMhRoCZhp30ty+XIRYVscADEjKam1mNhvuru7bzUkqLKt6PoSyFiyb3hWoq/6p/nP/Cvod4M+AYycVdHoAAAAAElFTkSuQmCC");          
           var options = {
             sourceType: 1,
             quality: 60,
-            destinationType: Camera.DestinationType.FILE_URI
+            destinationType: Camera.DestinationType.DATA_URL
           };
           navigator.camera.getPicture(uploadPhoto,null,options);
         }
@@ -117,7 +130,9 @@ angular.module('projectmgrApp')
       };
 
       function uploadPhoto(fileUrl){
-        answersToUpdate.url = fileUrl;
+
+        alert(fileUrl);
+        newUrl = fileUrl;
       }
 
       function resetView() {
